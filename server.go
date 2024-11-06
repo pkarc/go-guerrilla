ad0d56ec3173fa3dea3797a85ea7b575d6a7394c
pkarc <ivanchojara@gmail.com>
2024-11-06 15:36:17 -0500

    add filds to warn

--- server.go
+++ server.go
@@ -572,10 +572,10 @@ func (s *server) handleClient(client *client) {
 				if client.errors >= MaxUnrecognizedCommands {
 					client.sendResponse(r.FailMaxUnrecognizedCmd)
 					client.kill()
-					s.log().Errorf("Client [%s] sent too many unrecognized commands: %s", client.RemoteIP, cmd)
+					s.log().WithFields(logrus.Fields{"ip": client.RemoteIP, "cmd": cmd}).Warn("Client sent too many unrecognized commands")
 				} else {
 					client.sendResponse(r.FailUnrecognizedCmd)
-					s.log().Errorf("Client [%s] sent an unrecognized command: %s", client.RemoteIP, cmd)
+					s.log().WithFields(logrus.Fields{"ip": client.RemoteIP, "cmd": cmd}).Warn("Client sent an unrecognized command")
 				}
 			}
 
